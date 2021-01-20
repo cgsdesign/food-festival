bd8e041b09d3c09ef3c58d2378a6b8c8a8172a1e
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-01-20 16:26:26 -0600

    split up JS by page

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,35 @@
 //For a basic configuration, we need to provide webpack with three properties: entry, output, and mode. 
 //** NOte- if I am using JQuery etc. It is a dependency and will need to be added */
         //npm i jquery
-
+//Time to Interactive = (TTI)
 //npm run build
+//npm install -D webpack-bundle-analyzer 
 
-
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
 
-
-
-
-
-
-
-
-
-
-
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-      },
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
+  output: {
+    filename: "[name].bundle.js",//[name] specifies the file anme that we have in out JS that is bundled
+    path: __dirname + "/dist",
+  },
       //dependent plugins here
       plugins: [
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-          }),
-      ],
+          $: "jquery",
+          jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+      ],  
       mode: 'development'
 
 };
