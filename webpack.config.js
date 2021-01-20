9fa6b4238f61586985323e1a0da16ebd3fcb1476
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-01-20 17:05:52 -0600

    cleared code for start of 9.4 - left notes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,20 @@
 //For a basic configuration, we need to provide webpack with three properties: entry, output, and mode. 
 //** NOte- if I am using JQuery etc. It is a dependency and will need to be added */
         //npm i jquery
-//Time to Interactive = (TTI)
+//Optimize JS
+        //npm install -D webpack-bundle-analyzer =Makes it easier to analize how the JS is getting split and how much it is saving us
+//Optimize images
+        //npm install -D file-loader
+        //npm i image-webpack-loader  - makes images optimized
 //npm run build
-//npm install -D webpack-bundle-analyzer 
+
+//VOCAB:
+//Time to Interactive = (TTI)
+//Loaders pre-process the assets on a file by file basis before and during the build
+  //Loaders are used mostly for non JavaScript files such as image, CSS, and HTML files.
+//Plugins are scoped more on the bundle level and deliver changes at the end of the build
+  //While loaders are configured in the module property of the webpack configuration object, plugins are configured in the plugins array.
+
 
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
@@ -23,6 +34,33 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",//[name] specifies the file anme that we have in out JS that is bundled
     path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        //above ideonifies .jpg files etc
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name (file) {
+                return "[path][name].[ext]"
+              },
+              //above allows the files to keep an understandable filename
+              //below puts the files into a dist subfolder
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }  
+          },
+          {
+            //optimise file size (note load first THEN optimize)
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
       //dependent plugins here
       plugins: [
